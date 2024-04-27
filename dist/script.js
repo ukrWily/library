fd0043953d65a8a35e06794921973ac1815e2b2d
ukrWily <ukrwily@gmail.com>
2024-04-27 11:55:54 +0300

    L Added find

--- dist/script.js
+++ dist/script.js
@@ -117,15 +117,29 @@ _core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.index = function () {
 };
 
 /**
- * return index of elem in block
+ * find and return obj sorted by selector
  */
-_core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.index = function () {
-  const parent = this[0].parentNode;
-  const childs = [...parent.children];
-  const findMyIndex = item => {
-    return item == this[0];
-  };
-  return childs.findIndex(findMyIndex);
+_core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.find = function (selector) {
+  let numberOfItems = 0;
+  let counter = 0;
+  const copyObj = Object.assign({}, this);
+  for (let i = 0; i < copyObj.length; i++) {
+    const arr = copyObj[i].querySelectorAll(selector);
+    if (arr.length == 0) {
+      continue;
+    }
+    for (let j = 0; j < arr.length; j++) {
+      this[counter] = arr[j];
+      counter++;
+    }
+    numberOfItems += arr.length;
+  }
+  this.length = numberOfItems;
+  const objLength = Object.keys(this).length;
+  for (; numberOfItems < objLength; numberOfItems++) {
+    delete this[numberOfItems];
+  }
+  return this;
 };
 
 /***/ }),
--- src/js/lib/modules/actions.js
+++ src/js/lib/modules/actions.js
@@ -45,15 +45,34 @@ $.prototype.index = function () {
 };
 
 /**
- * return index of elem in block
+ * find and return obj sorted by selector
  */
-$.prototype.index = function () {
-  const parent = this[0].parentNode;
-  const childs = [...parent.children];
+$.prototype.find = function (selector) {
+  let numberOfItems = 0;
+  let counter = 0;
 
-  const findMyIndex = (item) => {
-    return item == this[0];
-  };
+  const copyObj = Object.assign({}, this);
 
-  return childs.findIndex(findMyIndex);
+  for (let i = 0; i < copyObj.length; i++) {
+    const arr = copyObj[i].querySelectorAll(selector);
+    if (arr.length == 0) {
+      continue;
+    }
+
+    for (let j = 0; j < arr.length; j++) {
+      this[counter] = arr[j];
+      counter++;
+    }
+
+    numberOfItems += arr.length;
+  }
+
+  this.length = numberOfItems;
+
+  const objLength = Object.keys(this).length;
+  for (; numberOfItems < objLength; numberOfItems++) {
+    delete this[numberOfItems];
+  }
+
+  return this;
 };
