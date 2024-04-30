6f62598910677910a5e037e692869c6dd49e2962
”ukrWily” <ukrwily@gmail.com>
2024-04-30 20:21:42 +0300

    PC styles

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,17 @@
 import $ from "./lib/lib";
 
+$("#first").on("click", () => {
+  $("div").eq(0).fadeOut(800);
+});
+$('[data-count="second"]').on("click", () => {
+  $("div").eq(1).fadeOut(800);
+});
+$("button")
+  .eq(2)
+  .on("click", () => {
+    $(".w-500").fadeOut(800);
+  });
+
 // $("button").on("click", function () {
 //   $(".wrapper>div").toggleClass("active");
 // });
