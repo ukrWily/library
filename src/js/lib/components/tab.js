6ebda9cb4faf8983155d80df988268888f7e0361
”ukrWily” <ukrwily@gmail.com>
2024-05-07 21:28:54 +0300

    PC accordion

--- src/js/lib/components/tab.js
+++ src/js/lib/components/tab.js
@@ -4,11 +4,16 @@ $.prototype.tab = function () {
   for (let i = 0; i < this.length; i++) {
     $(this[i]).on("click", () => {
       $(this[i])
-        .addClass("tab-item--active")
-        .siblings()
-        .removeClass("tab-item--active");
+        .addClass("tab-item--active") // Add class to target
+        .siblings() // get sibling
+        .removeClass("tab-item--active") // And remove of them class
+        .closest(".tab") // get closest tab
+        .find(".tab-content") // find tab-content into tab
+        .removeClass("tab-content--active") //remove active class of all
+        .eq($(this[i]).index()) // get active tab number of line
+        .addClass("tab-content--active"); // And add class to active tab
     });
   }
 };
 
-$("[data-panel .tab-item").tab();
+$("[data-tabpanel] .tab-item").tab();
